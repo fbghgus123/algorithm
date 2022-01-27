7d111110a6f1818d99189a9f7836ceea8ce3ba95
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-01-28 00:25:40 +0900

    Solve 2579

---
+++ 백준/DP/2579_계단오르기.py
@@ -0,0 +1,18 @@
+# 문제: https://www.acmicpc.net/problem/2579
+# 도움된 글: https://sungmin-joo.tistory.com/18
+
+import sys
+input = sys.stdin.readline
+
+l = int(input())
+arr = [int(input()) for _ in range(l)]
+
+dp = [arr[0]]
+dp.append(max(arr[0]+arr[1], arr[1]))
+dp.append(max(arr[0]+arr[2], arr[1]+arr[2]))
+for i in range(3, l):
+    dp.append(max(dp[i-2] + arr[i], dp[i-3] + arr[i] + arr[i-1]))
+
+print(dp.pop())
+
+
--- 백준/DP/2631_줄세우기.py
+++ 백준/DP/2631_줄세우기.py
@@ -4,14 +4,9 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-dp = [(0, 0)]*(N+1)
-print(dp)
-_max = 0
-for i in range(1, N+1):
-    n = int(input())
-    for j in range(i):
-        if dp[j][0]<n :
-            dp[i] = (n, max(dp[j][1]+1, dp[i][1]))
-            _max = max(_max, dp[i][1])
-print(N-_max)
+n = int(input())
+line = [int(input()) for _ in range(n)]
+dp = []
+
+for i in line:
+    dp.append(i)
