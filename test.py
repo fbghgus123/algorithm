22206a69ab901566e420fab7cede210b2f869551
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-02-18 21:44:37 +0900

    Solve problem

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-import math
-n, kim, lim = map(int, input().split())
-for i in range(math.ceil(n ** 0.5)):
-    if min(kim, lim) % 2 == 1 and abs(kim - lim) == 1:
-        a = False
-        print(i + 1)
-        break
-    else:
-        kim = math.ceil(kim/2)
-        lim = math.ceil(lim/2)
+print("""\    /\\
+ )  ( ')
+(  /  )
+ \(__)|""")
