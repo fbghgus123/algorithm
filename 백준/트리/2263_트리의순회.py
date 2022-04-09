2aba6618f036c2eeaca22929eeec19211ebd0f88
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-04-09 17:21:47 +0900

    Solve problem

--- 백준/트리/2263_트리의순회.py
+++ 백준/트리/2263_트리의순회.py
@@ -1,3 +1,5 @@
+# 문제 : https://www.acmicpc.net/problem/2263
+
 import sys
 sys.setrecursionlimit(100000)
 
@@ -6,20 +8,17 @@ inorder = input().split()
 postorder = input().split()
 
 answer = []
-count = 0
 def divide(si, ei, sp, ep):
     global count
     if si <= ei and sp <= ep:
-        count +=1
-        if count > 10: return
         root = postorder[ep]
         answer.append(root)
         if si < ei and sp < ep:
             endIn = inorder.index(root)
             endPost = sp + (endIn-si-1)
-            print(root)
-            print(inorder[si:endIn], inorder[sp:endPost+1])
-            print(postorder[endIn+1:ei+1], postorder[endPost+1:ep])
+            # print(root)
+            # print(inorder[si:endIn], inorder[sp:endPost+1])
+            # print(postorder[endIn+1:ei+1], postorder[endPost+1:ep])
             divide(si, endIn-1, sp, endPost)
             divide(endIn+1, ei, endPost+1, ep-1)
             
