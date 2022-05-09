66002077f8a622f7e48912c234f83b2553cec68b
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-05-09 17:05:17 +0900

    Solve 2 problems

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-N, K = map(int, input().split())
-b = []
-for i in range(N):
-    b.append(int(input()))
-total = 0
-b.reverse()
-
-for i in range(N):
-    if K >= b[i]:
-        a = K // b[i]
-        K = K % b[i]
-        total += a
-print(total)
+(SELECT * FROM CART_PRODUCTS group by CART_ID, NAME) as a,
