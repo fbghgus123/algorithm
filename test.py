269ec94fc3572f1b1c0d6ac1493c155612645521
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-06-29 22:53:59 +0900

    Solve problem

--- test.py
+++ test.py
@@ -1,33 +1,37 @@
-from collections import defaultdict, deque
+from audioop import reverse
+from collections import deque
+n, m, v = map(int, input().split())
 
-n = int(input())
-greed = [0]
-cost = [0]
-effect = defaultdict(list)
-
-for i in range(1, n+1):
-    t, c, *w = map(int, input().split())
-    cost.append(t)
-    greed.append(c)
-    for need in w:
-        effect[need].append(i)
-
-time = [0] * (n+1)
-queue = deque()
-for i in range(1, n+1):
-    if greed[i] == 0: queue.append(i)
-
-while queue:
-    now = queue.popleft()
-    for i in effect[now]:
-        greed[i] -= 1
-        time[i] = max(time[i], time[now] + cost[now])
-        if greed[i] == 0:
-            queue.append(i)
-
-answer = []
-for i in range(1, n+1):
-    answer.append(time[i] + cost[i])
-print(max(answer))
+path = [[] for _ in range(n+1)]
+for _ in range(m):
+    s, e = map(int, input().split())
+    path[s].append(e)
+    path[e].append(s)
 
 
+def dfs(v):
+    stack = [v]
+    visited = []
+    while(stack):
+        current = stack.pop()
+        if current in visited: continue
+        visited.append(current)
+        for next in path[current]:
+            stack.append(next)  
+    return visited
+
+def bfs(v):
+    queue = deque([v])
+    visited = [v]
+    while (queue):
+        current = queue.popleft()
+        for next in path[current]:
+            if next in visited: continue
+            queue.append(next)
+            visited.append(next)
+    return visited
+
+[i.sort(reverse=True) for i in path]
+print(*dfs(v))
+[i.sort() for i in path]
+print(*bfs(v))
