16d01c96df11df01a0f97d5d7cbfad013873cb70
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-07-09 15:04:47 +0900

    Solve problem

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,120 +1,62 @@
 #include <stdio.h>
-#include <string>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
 int N;
-int height[50][50];
-int low = 1000000; int high = 0;
-int answer = 1000000;
-pair<int, int> home;
-vector<pair<int, int>> target;
 
-int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+pair<int, int> arr[1000];
+int p = 0;
 
-bool bfs(int minn, int maxx) {
-    if (minn > height[home.first][home.second]) return false;
-    int visited[50][50];
-    for (int i=0; i<50; i++) for(int j=0; j<50; j++) visited[i][j] = 0;
-    queue<pair<int ,int>> q;
-    q.push(home);
-
-    while (!q.empty()) {
-        pair<int, int> tmp = q.front();
-        q.pop();
-
-        int y = tmp.first;
-        int x = tmp.second;
-        for (int i=0; i<8; i++) {
-            int cy = y + dy[i];
-            int cx = x + dx[i];
-            // 범위 확인
-            if (0 <= cy && 0 <= cx && cy < N && cx < N) {
-                // 방문 가능 체크
-                if (visited[cy][cx] == 0 && height[cy][cx] >= minn && height[cy][cx] <= maxx) {
-                    visited[cy][cx] = 1;
-                    q.push({cy, cx});
-                }
-            }
+int find_less(int myday) {
+    int result = 0;
+    for (int i=0; i<p; i++) {
+        if (arr[i].first <= myday) {
+            result++;
         }
     }
-    for (int i=0; i<target.size(); i++) {
-        pair<int, int> t = target[i];
-        if (!visited[t.first][t.second]) {
-            return false;
-        };
-    }
-    return true;
+    return result;
 }
 
-int find_maxx(int l) {
-    int r = high+1;
-    int mid;
-    int answer = 1000001;
-    while (l <= r) {
-        mid = (l + r) / 2;
-        if (bfs(low, mid)) {
-            if (answer > mid) answer = mid;
-            r = mid - 1;
-        }
-        else {
-            l = mid + 1;
-        }
+struct cmp {
+    bool operator()(pair<int, int> a, pair<int, int> b) {
+        return a.second < b.second;
     }
-    return answer;
-}
+};
 
-int find_minn(int r) {
-    int l = low;
-    int answer = l, mid;
-    while (l <= r) {
-        mid = (l + r) / 2;
-        if (bfs(mid, r)) {
-            answer = mid;
-            l = mid + 1;
-        }
-        else {
-            r = mid - 1;
-        }
-    }
-    return answer;
-}
-
-void find_answer() {
-    int l; int h;
-    while (low <= high) {
-        h = find_maxx(low);
-        if (h == 1000001) break;
-        l = find_minn(h);
-        int tmp = h - l;
-        if (tmp < answer) answer = tmp;
-        
-        high = h;
-        low = l + 1;
-    }
-    printf("%d\n", answer);
-}
+priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> q;
 
 int main() {
     scanf("%d", &N);
     for (int i=0; i<N; i++) {
-        char str[N];
-        scanf("%s", str);
-        for (int j=0; j<N; j++) {
-            if (str[j] == 'K') target.push_back({i, j});
-            if (str[j] == 'P') home = {i, j};
+        pair<int, int> tmp;
+        scanf("%d %d",&tmp.first, &tmp.second);
+        q.push(tmp);
+    }
+    int day = 1, answer = 0;
+    while (!q.empty()) {
+        pair<int, int> tmp = q.top();
+        q.pop();
+
+        if (tmp.first - find_less(tmp.first) > day && q.size() > 0) {
+            arr[p++] = tmp;
+        }
+        else {
+            if (tmp.first >= day) {
+                answer += tmp.second;
+                day ++;
+                while (p > 0) {
+                    q.push(arr[--p]);
+                }
+            }
+        }
+        if (q.empty() && p > 0) {
+            while (p > 0) {
+                q.push(arr[--p]);
+            }
         }
     }
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<N; j++) {
-            scanf("%d", &height[i][j]);
-            if (low > height[i][j]) low = height[i][j];
-            if (high < height[i][j]) high = height[i][j];
-        }
-    }
-    find_answer();
+    printf("%d\n", answer);
     return 0;
 }
